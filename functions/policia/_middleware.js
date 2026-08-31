1bdd72f3958b9cb64f541602a01a5550701339d8
martinesP0923 <martinesnar845@gmail.com>
2026-08-31 02:58:20 +0000

    reemplazar contenido de policia

--- functions/policia/_middleware.js
+++ functions/policia/_middleware.js
@@ -17,7 +17,15 @@ export async function onRequest({ request, next, env }) {
   const tieneAcceso = session.roles.some((r) => allowedRoles.includes(r));
 
   if (!tieneAcceso) {
-    return Response.redirect(`${url.origin}/?acceso=denegado`, 302);
+    return new Response(
+      JSON.stringify({
+        env_crudo: env.POLICIA_ROLE_IDS,
+        allowedRoles,
+        sessionRoles: session.roles,
+        tieneAcceso,
+      }, null, 2),
+      { headers: { "Content-Type": "application/json" } }
+    );
   }
 
   return next();
